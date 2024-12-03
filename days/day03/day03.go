9c05cc3818c44e67bf02049d42653bd22fe2c8ae
Lukas Jenicek <lukas.jenicek5@gmail.com>
2024-12-03 19:52:11 +0100

    day03 part2 finished

--- days/day03/day03.go
+++ days/day03/day03.go
@@ -52,5 +52,58 @@ func (*ThirdDay) RunFirstPart(input string) (string, error) {
 }
 
 func (*ThirdDay) RunSecondPart(input string) (string, error) {
-	return "", nil
+	re := regexp.MustCompile(`do\(\)|don't\(\)|mul\([0-9]+,[0-9]+\)`)
+	multiplications := re.FindAllStringSubmatch(input, -1)
+
+	var result int64
+	do := true
+	for _, multiplication := range multiplications {
+		mul := multiplication[0]
+
+		if mul == "don't()" {
+			do = false
+			continue
+		}
+
+		if mul == "do()" {
+			do = true
+			continue
+		}
+
+		if !do {
+			continue
+		}
+
+		parsingSecondNumber := false
+		firstNumber := ""
+		secondNumber := ""
+		for _, m := range mul {
+			if unicode.IsDigit(m) {
+				if parsingSecondNumber {
+					secondNumber += string(m)
+					continue
+				}
+
+				firstNumber += string(m)
+			}
+
+			if m == ',' {
+				parsingSecondNumber = true
+			}
+		}
+
+		fNumber, err := strconv.ParseInt(firstNumber, 10, 64)
+		if err != nil {
+			return "", fmt.Errorf("could not parse first number: %w", err)
+		}
+
+		sNumber, err := strconv.ParseInt(secondNumber, 10, 64)
+		if err != nil {
+			return "", fmt.Errorf("could not parse second number: %w", err)
+		}
+
+		result += fNumber * sNumber
+	}
+
+	return fmt.Sprintf("%v", result), nil
 }
